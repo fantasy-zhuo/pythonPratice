3ff96709e0457068cf9d37e8ffd1d1fca6941247
zhou12452 <1316889146@qq.com>
2021-01-21 13:49:02 +0800

    commit

--- oss/ossDemo.py
+++ oss/ossDemo.py
@@ -14,10 +14,6 @@ import oss2
 # # 3. 解析响应
 # print(res.text)  # 输出响应的文本
 #
-bucket_name = 'instai-wh'
-endpoint = 'http://oss-cn-chengdu.aliyuncs.com'
-# 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-auth = oss2.Auth(access_key_id, access_key_secret)
 # Endpoint以杭州为例，其它Region请按实际情况填写。
 bucket = oss2.Bucket(auth, endpoint, bucket_name)
 
